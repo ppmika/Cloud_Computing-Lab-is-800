ebf8d7a9eb3a6545dca44607e60751c6cfaf0f3a
mikapp <111213082+ppmika@users.noreply.github.com>
2025-04-03 13:26:48 -0400

    Add files via upload
    Using Loops in Terraform

--- main.tf
+++ main.tf
@@ -1,18 +1,37 @@
-terraform {
-  backend "s3" {
-    bucket         = "tartela-bucket"
-    key            = "terraform/myapp/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "terraform-lock"
-  }
+##First Part/*
+provider "aws" {
+  region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami           ="ami-00110730b7437ee9e"# Replace with correct AMI ID
+resource "aws_instance" "web" {
+  count = 3  # Creates 3 instances
+
+  ami           = "ami-00110730b7437ee9e"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "Terraform-Test-Instance"
+    Name = "Terraform-Instance-${count.index}"
+  }
+}
+*/
+
+##2nd part
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "instance_types" {
+  type    = list(string)
+  default = ["t2.micro", "t3.micro", "t2.small"]
+}
+
+resource "aws_instance" "web" {
+  count = length(var.instance_types)  # Dynamically set based on the list size
+
+  ami           = "ami-00110730b7437ee9e"
+  instance_type = var.instance_types[count.index]
+
+  tags = {
+    Name = "Terraform-Instance-${count.index}"
   }
 }
